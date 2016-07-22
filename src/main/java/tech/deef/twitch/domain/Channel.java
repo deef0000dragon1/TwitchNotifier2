037426ff06c5b8443cb9cf082dea9aa97e775127
deef <deef551@gmail.com>
2016-07-22 06:30:21 -0500

    add ignoreUnknown to Channel and ChannelsUser

--- src/main/java/tech/deef/twitch/domain/Channel.java
+++ src/main/java/tech/deef/twitch/domain/Channel.java
@@ -1,7 +1,9 @@
 package tech.deef.twitch.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Channel {
 
 	@JsonProperty("_links")
--- src/main/java/tech/deef/twitch/domain/ChannelsUser.java
+++ src/main/java/tech/deef/twitch/domain/ChannelsUser.java
@@ -1,5 +1,8 @@
 package tech.deef.twitch.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ChannelsUser extends Channel{
 	//this class exists sole for clarity purposes 
 	//as all JSON Fields are handled by Channel
